2b5a39dce8a6cd0b71cf14c25e1b3b86fc26d9aa
mohamed jamoun <mjamoun28@gmail.com>
2022-11-07 20:44:53 +0100

    create the other api calls

--- pages/index.vue
+++ pages/index.vue
@@ -7,17 +7,44 @@
 
       <v-tab-item>
         <v-card flat>
-          <v-card-text> </v-card-text>
+          <v-card-text>
+            <h2 class="mt-2">Branch Details</h2>
+            <v-card outlined class="mt-5">
+              <v-data-table
+                hide-default-footer
+                :headers="branchesHeaders"
+                :items="localBranches"
+              />
+            </v-card>
+          </v-card-text>
         </v-card>
       </v-tab-item>
       <v-tab-item>
         <v-card flat>
-          <v-card-text> </v-card-text>
+          <v-card-text>
+            <h2 class="mt-2">Stuff</h2>
+            <v-card outlined class="mt-5">
+              <v-data-table
+                hide-default-footer
+                :headers="stuffHeaders"
+                :items="stuff"
+              />
+            </v-card>
+          </v-card-text>
         </v-card>
       </v-tab-item>
       <v-tab-item>
         <v-card flat>
-          <v-card-text> </v-card-text>
+          <v-card-text>
+            <h2 class="mt-2">Services</h2>
+            <v-card outlined class="mt-5">
+              <v-data-table
+                hide-default-footer
+                :headers="servicesHeaders"
+                :items="services"
+              />
+            </v-card>
+          </v-card-text>
         </v-card>
       </v-tab-item>
     </v-tabs>
@@ -25,14 +52,107 @@
 </template>
 
 <script>
-import mapState from "vuex";
 export default {
   name: "IndexPage",
-  fetch() {
-    this.$store.dispatch("fetchBranches");
+  created() {
+    // login
+    this.$store
+      .dispatch("login", {
+        email: "test@test.com",
+        password: "test@test",
+      })
+      .then(() => {
+        this.$store.dispatch("getBranches");
+        this.$store.dispatch("getStuff");
+        this.$store.dispatch("getServices");
+      });
+  },
+  data() {
+    return {
+      branchesHeaders: [
+        {
+          text: "Branch ID",
+          align: "start",
+          value: "id",
+        },
+        { text: "Country", value: "country" },
+        { text: "City", value: "city" },
+        { text: "District", value: "district" },
+      ],
+      stuffHeaders: [
+        {
+          text: "Stuff ID",
+          align: "start",
+          value: "id",
+        },
+        { text: "Name", value: "name" },
+        { text: "Surname", value: "surname" },
+        { text: "Position", value: "position" },
+      ],
+      servicesHeaders: [
+        {
+          text: "Service ID",
+          align: "start",
+          value: "id",
+        },
+        { text: "Name", value: "name" },
+        { text: "Price", value: "price" },
+        { text: "Duration", value: "duration" },
+      ],
+      localBranches: [
+        {
+          id: 1,
+          country: "Turkey",
+          city: "Istanbul",
+          district: "Kadıköy",
+        },
+        {
+          id: 2,
+          country: "Morocco",
+          city: "Rabat",
+          district: "sala",
+        },
+      ],
+      stuff: [
+        {
+          id: 1,
+          name: "John",
+          surname: "Doe",
+          position: "Manager",
+        },
+        {
+          id: 2,
+          name: "Jane",
+          surname: "Doe",
+          position: "Manager",
+        },
+      ],
+      services: [
+        {
+          id: 1,
+          name: "Haircut",
+          price: 20,
+          duration: 30,
+        },
+        {
+          id: 2,
+          name: "Haircut",
+          price: 20,
+          duration: 30,
+        },
+      ],
+    };
   },
   computed: {
-    ...mapState(["branches"]),
+    branches() {
+      return this.$store.state.branches;
+    },
+    businessUsers() {
+      return this.$store.state.businessUsers;
+    },
+    serviceCategories() {
+      return this.$store.state.serviceCategories;
+    },
   },
 };
 </script>
--- store/index.js
+++ store/index.js
@@ -1,16 +1,52 @@
 export const state = () => ({
-  branches: 0,
+  branches: [],
+  businessUsers: [],
+  serviceCategories: [],
 });
 
 export const mutations = {
   setBranches(state, branches) {
     state.branches = branches;
   },
+  setBusinessUsers(state, businessUsers) {
+    state.businessUsers = businessUsers;
+  },
+  setServiceCategories(state, serviceCategories) {
+    state.serviceCategories = serviceCategories;
+  },
 };
 
 export const actions = {
+  async login({ commit }, { email, password }) {
+    try {
+      const formData = new FormData();
+      formData.append("email", email);
+      formData.append("password", password);
+
+      const { data } = await this.$axios.post("/admin/login", formData);
+
+      this.$axios.setToken(data.token, "Bearer");
+    } catch (error) {
+      console.log("error", error);
+    }
+  },
   async getBranches({ commit }) {
     const branches = await this.$axios.$get("/admin/business/1/branches");
+
     commit("setBranches", branches);
+  },
+  async getBusinessUsers({ commit }) {
+    const businessUsers = await this.$axios.$get(
+      "/admin/business/1/business-users"
+    );
+
+    commit("setBusinessUsers", businessUsers);
+  },
+  async getServiceCategories({ commit }) {
+    const serviceCategories = await this.$axios.$get(
+      "/admin/business/1/service-categories"
+    );
+
+    commit("setServiceCategories", serviceCategories);
   },
 };
